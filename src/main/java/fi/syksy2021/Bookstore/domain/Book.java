73c933fb9cc64b1f206b5afa73c40a990231a7eb
Elina Putus <elina.putus@digitalworkforce.com>
2021-09-05 17:22:21 +0300

    Ensimmäinen versio

---
+++ src/main/java/fi/syksy2021/Bookstore/domain/Book.java
@@ -0,0 +1,12 @@
+package fi.syksy2021.Bookstore.domain;
+
+public class Book {
+	
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private double price;
+	
+
+}
