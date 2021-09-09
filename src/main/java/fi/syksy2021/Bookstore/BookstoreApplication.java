3b01159e36240659332a380a19a968baaa9e582a
Elina Putus <elina.putus@digitalworkforce.com>
2021-09-09 16:39:00 +0300

    JAP ja H2

--- src/main/java/fi/syksy2021/Bookstore/BookstoreApplication.java
+++ src/main/java/fi/syksy2021/Bookstore/BookstoreApplication.java
@@ -1,13 +1,28 @@
 package fi.syksy2021.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.syksy2021.Bookstore.domain.Book;
+import fi.syksy2021.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner bookStore(BookRepository bookRepository) {
+		return (args) -> {
+			bookRepository.save(new Book("Uuni", "Antti Tuuri", 2010, "abc12345676", 10.00));
+			for (Book book: bookRepository.findAll()) {
+				System.out.println(book.toString());
+			}
+		};
 	}
 
 }
