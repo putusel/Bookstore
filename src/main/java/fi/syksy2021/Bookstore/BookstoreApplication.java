aee52a14136963c42cfd28d41cf1cb1d4c871ba3
putusel <elina.putus@gmail.com>
2021-09-12 09:53:33 +0300

    Viikkotehtävä 2 Bookstore Listpage

--- src/main/java/fi/syksy2021/Bookstore/BookstoreApplication.java
+++ src/main/java/fi/syksy2021/Bookstore/BookstoreApplication.java
@@ -18,7 +18,8 @@ public class BookstoreApplication {
 	@Bean
 	public CommandLineRunner bookStore(BookRepository bookRepository) {
 		return (args) -> {
-			bookRepository.save(new Book("Uuni", "Antti Tuuri", 2010, "abc12345676", 10.00));
+			bookRepository.save(new Book("A Farewell to Arms", "Ernest Hemingway", 1929, "1232323-21", 25.00));
+			bookRepository.save(new Book("Animal Farm", "George Orwell", 1945, "2212343-5", 21.00));
 			for (Book book: bookRepository.findAll()) {
 				System.out.println(book.toString());
 			}
--- src/main/java/fi/syksy2021/Bookstore/web/BookController.java
+++ src/main/java/fi/syksy2021/Bookstore/web/BookController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import fi.syksy2021.Bookstore.domain.BookRepository;
 
 
-	@Controller
-	public class BookController {
+@Controller
+public class BookController {
 	
 	@Autowired
 	private BookRepository repository; 
@@ -22,7 +22,7 @@ import fi.syksy2021.Bookstore.domain.BookRepository;
         return "XXXX";
     
     }
-    @RequestMapping(value= {"/", "/booklist"})
+    @RequestMapping(value= "/booklist")
     public String bookList(Model model) {
     	model.addAttribute("books", repository.findAll());
     	return "booklist";
