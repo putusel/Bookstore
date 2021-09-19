e393e177c4d97bbbe1ed07ad8711b1044b5daf82
putusel <elina.putus@gmail.com>
2021-09-19 11:31:05 +0300

    Rest

--- src/main/java/fi/syksy2021/Bookstore/web/BookController.java
+++ src/main/java/fi/syksy2021/Bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package fi.syksy2021.Bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import fi.haagahelia.course.domain.Student;
 import fi.syksy2021.Bookstore.domain.Book;
 import fi.syksy2021.Bookstore.domain.BookRepository;
 import fi.syksy2021.Bookstore.domain.CategoryRepository;
@@ -34,6 +38,18 @@ public class BookController {
     	model.addAttribute("books", repository.findAll());
     	return "booklist";
     }
+    
+    // RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+	// RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findStudentRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    }       
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
